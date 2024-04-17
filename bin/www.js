f224f035627b9024c6a0c60424638a5a27c11316
davidtomeyan <79004506+davidtomeyan@users.noreply.github.com>
2024-04-17 18:26:13 +0300

    Update www.js

--- bin/www.js
+++ bin/www.js
@@ -3,7 +3,7 @@ import app from "../src/app.js";
 import 'dotenv/config'
 
 const port = process.env.PORT||8080
-const connectionString = process.env.DB_URI || "";   
+const connectionString = 'mongodb://gen_user:XwKela%25Bz29A)(@147.45.105.183:27017/default_db?authSource=admin&directConnection=true';   
 async function main() {
     try {
         await mongoose.connect(connectionString)
